21cd683df4ff513bfe32fc9e1284944f4fda187a
alvarofeher <alvarofeherargullos@gmail.com>
2026-08-01 17:36:18 +0200

    Initial Commit

---
+++ TestSwiftAiProject/TestSwiftAiProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftAiProjectApp.swift
+//  TestSwiftAiProject
+//
+//  Created by Hola Buenas tardes on 8/1/26.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftAiProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
